71b23f2e0e9fbc2ae9dd61ec61a4869392085d1d
jwhuettl <jwhuettl@gmail.com>
2026-03-06 17:34:06 -0600

    Adding redirect and rewording header

--- javascripts/discourse/api-initializers/body-class.js
+++ javascripts/discourse/api-initializers/body-class.js
@@ -3,5 +3,11 @@ import { apiInitializer } from "discourse/lib/api";
 export default apiInitializer((api) => {
   api.onPageChange(() => {
     document.body.classList.add("sparq-theme");
+
+    if (document.querySelector('.page-not-found') && !Discourse.User.current()) {
+      window.location.href = '/login';
+    }
   });
+
+  
 });
