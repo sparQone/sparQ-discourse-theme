990e2a2e892fb69fdd9d831e61a5a1c5fd505808
jwhuettl <jwhuettl@gmail.com>
2026-03-06 17:38:00 -0600

    testing js

--- javascripts/discourse/api-initializers/body-class.js
+++ javascripts/discourse/api-initializers/body-class.js
@@ -4,7 +4,10 @@ export default apiInitializer((api) => {
   api.onPageChange(() => {
     document.body.classList.add("sparq-theme");
 
+    console.log('before refresh');
+
     if (document.querySelector('.page-not-found') && !Discourse.User.current()) {
+      console.log('refresh here');
       window.location.href = '/login';
     }
   });
